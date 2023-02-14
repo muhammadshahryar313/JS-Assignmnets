a2b9a7e6f85691be8b637734e63cc8016938097f
Shahryar <120104011+muhammadshahryar313@users.noreply.github.com>
2023-02-14 19:02:46 +0500

    Add files via upload

---
+++ Javascript/Js Assignment  5 (14-16)/app.js
@@ -0,0 +1,87 @@
+// Chapter 14
+// Q1
+
+// var fruits = [];
+
+
+// Q2
+
+// var studentNames = [];
+// // console.log(studentNames);
+
+// Q3
+
+// var studentNames = ["Ali", "Ahmad", "Khan"];
+// console.log(studentNames);  
+
+
+// Q4
+
+// var numbers = [1, 2, 3];
+// console.log(numbera);
+
+// Q5
+
+// var strings = ["apple", "banana", "cherry"];
+// console.log(strings);
+
+// Q6
+// var booleans = [true, false, true];
+// console.log(booleans);
+
+// Q6
+
+// var mixed = [1, "hello", true, ["apple", "banana"]];
+// console.log(mixed);
+
+// Q7
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// var qualificationString = "1) " + qualifications[0] + "<br> 2) " + qualifications[1] + "<br> 3) " + qualifications[2] + "<br> 4) " + qualifications[3] + "<br> 5) " + qualifications[4] + "<br> 6) " + qualifications[5] + "<br> 7) " + qualifications[6] + "<br> 8) " + qualifications[7];
+
+// document.write("Qualifications:<br>");
+// document.write(qualificationString);
+
+//  Q8
+
+// var names = ["Ali", "Ahmad", "Khan"];
+// var scores = [400, 350, 450];
+// var totalMarks = 500;
+
+// var AliPercentage = (scores[0] / totalMarks) * 100;
+// var AhmadPercentage = (scores[1] / totalMarks) * 100;
+// var KhanPercentage = (scores[2] / totalMarks) * 100;
+
+// document.write("Score of " + names[0]+ " is " + scores[0] + " Percentage " + " (" + AliPercentage + "%)<br>");
+// document.write("Score of " + names[1]+ " is " + scores[1] + " Percentage " + " (" + AhmadPercentage + "%)<br>");
+// document.write("Score of " + names[2]+ " is " + scores[2] + " Percentage " + " (" + KhanPercentage + "%)<br>");
+
+
+// Q9
+// NOT DONE
+
+
+
+// Q10
+
+// var scores = [320, 230, 480, 120];
+
+
+// document.write("Scores of Students : " + scores); 
+// document.write("<br/> Ordered Scores of Students : " + scores.sort());
+
+
+// Q11
+
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// var selectedCities = [];
+// selectedCities.push(cities[2], cities[3]);
+
+// document.write("Cities List : <br/>" + cities + "<br/>" + "<br/>");
+
+// document.write("Selected Cities <br/>" + selectedCities);
+
+
+
+
