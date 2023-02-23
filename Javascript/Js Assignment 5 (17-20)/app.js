285476ef4be5dde4d5622b978a70689b892041f5
Shahryar <120104011+muhammadshahryar313@users.noreply.github.com>
2023-02-23 19:07:12 +0500

    Add files via upload

---
+++ Javascript/Js Assignment 5 (17-20)/app.js
@@ -0,0 +1,127 @@
+// Q1
+
+// var multiArray = [[]];
+
+// Q2
+
+// var matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1],
+//   ];
+//   console.log(matrix);
+
+// Q3
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+  
+
+// Q4
+
+// var number = +(prompt("Enter a table number:"));
+// var length = +(prompt("Enter the length of the table:"));
+// document.write("<h2> Multiplication table of " + number + " Length " + length + "</br> </h2>");
+// for (let i = 1; i <= length; i++) {
+//   document.write( number +' x ' + i + ' = ' + number * i + "</br>" );
+// }
+
+// Q5
+
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]) ;
+  
+ 
+// }
+
+// 6
+
+
+// document.write("<h3>Counting:</h3>");
+// for (var i = 1; i <= 15; i++) {
+//   document.write(i+ "  ");
+// }
+// document.write("<br>");
+
+
+// document.write("<h3>Reverse counting:</h3>");
+// for (var i = 10; i >= 1; i--) {
+//   document.write(i + "  ");
+// }
+// document.write("<br>");
+
+
+// document.write("<h3>Even numbers:</h3>");
+// for (var i = 0; i <= 20; i += 2) {
+//   document.write(i + '  ');
+// }
+// document.write("<br>");
+
+
+// document.write("<h3>Odd numbers:</h3>");
+// for (var i = 1; i <= 19; i += 2) {
+//   document.write(i  +  '  ');
+// }
+// document.write("<br>");
+
+
+// document.write("<h3>Series:</h3>");s
+// for (var i = 1; i <= 10; i++) {
+//   document.write(i + "k" + ' ');
+// }
+
+// Q7
+
+// var items = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt("Enter an item to search for:");
+
+// var foundIndex = items.findIndex((item) => item === userInput);
+
+// if (foundIndex !== -1) {
+//   alert(userInput + ' available at index ' + foundIndex + '  in bakery ');
+// } else {
+//   alert('We are sorry ' + userInput + ' not available in our bakery');
+// }
+
+
+// Q8
+
+
+// var numbers = [24, 53, 78, 91, 12, 100, 5];
+// var largestNumber = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > largestNumber) {
+//     largestNumber = numbers[i];
+//   }
+// }
+
+// document.write('The largest number in the array is ' + '<b>' +largestNumber);
+
+// Q9
+
+// var numbers = [24, 53, 78, 91, 12, 100, 5];
+// var smallestNumber = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+//   if (numbers[i] < smallestNumber) {
+//     smallestNumber = numbers[i];
+//   }
+// }
+
+// document.write('The smallest number in the array is ' + '<b>' +smallestNumber);
+
+// Q10
+
+
+// for (var i = 1; i <= 20; i++) {
+//     var multiple = i * 5;
+//     document.write(multiple + " ");
+//   }
+  
+
